d63b55beb4fcd681994a4a3402aaf5cf44812f39
raul.al.go96 <raul.al.go96@gmail.com>
2022-10-18 22:39:08 -0500

    Hiding localhost-key. updating database.

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+using System.Security.Cryptography;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+        }
+    }
+}
